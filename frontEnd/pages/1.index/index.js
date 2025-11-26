d5a396b2374dc7a9b1322f9667d9fc1c3b0c9509
Isabella Gonçalves Lopreti <lopreti@gmail.com>
2025-11-26 12:44:27 -0300

    comecando estruturacao das pastas

---
+++ frontEnd/pages/1.index/index.js
@@ -0,0 +1,57 @@
+const APILogin = "http://localhost:3000/login"
+const inputEmail = document.getElementById("email")
+const inputSenha = document.getElementById("senha")
+const btnEntrar = document.getElementById("btnEntrar")
+const checkboxLembrar = document.getElementById("remember")
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+});
+
+
+
+btnEntrar.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+
+    const email = inputEmail.value.trim();
+    const senha = inputSenha.value.trim();
+
+    if (!email || !senha) {
+        Toast.fire("Preencha todos os campos!");
+        return;
+    }
+    try {
+        const lembrar = checkboxLembrar.checked
+        const response = await fetch(APILogin, {
+            method: "POST",
+            credentials: "include",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, senha, lembrar })
+        })
+
+        const dado = await response.json();
+
+        if (!response.ok) {
+            Toast.fire(dado.erro || "Erro ao fazer login");
+            return;
+        }
+        const perfil = dado.aluno.perfil;
+
+        // Redirecionar
+        if (perfil === "aluno") {
+            window.location.href = "../5.Usuario/index.html";
+        } else {
+            window.location.href = "../2.Dashboard/index.html";
+        }
+
+    } catch (error) {
+        console.log("Erro:", error);
+        Toast.fire("Erro de conexão com o servidor.");
+    }
+
+})
