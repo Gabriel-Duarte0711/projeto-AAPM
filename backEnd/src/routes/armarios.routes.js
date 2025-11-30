01fe6268c44905df971bbbe36b1be729de92de3b
gabrielDuarte <biel111107@gmail.com>
2025-11-30 14:08:55 -0300

    nao deu certo o jwt

--- backEnd/src/routes/armarios.routes.js
+++ backEnd/src/routes/armarios.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { listarArmariosG, listarArmariosL, atualizarEstado } from "../controllers/armarios.controller.js";
-
+import { autenticarToken } from "file:///C:/Users/biel1/OneDrive/Area%20de%20Trabalho/projeto-AAPM/backend/src/middleware/auth.middleware.js";
 const router = express.Router();
 
-router.get("/", listarArmariosG);
-router.get("/obterUsuario", listarArmariosL);
-router.put("/:numero_armario", atualizarEstado)
+router.get("/",autenticarToken, listarArmariosG);
+router.get("/obterUsuario",autenticarToken, listarArmariosL);
+router.put("/:numero_armario",autenticarToken, atualizarEstado)
 
 
 export default router;
